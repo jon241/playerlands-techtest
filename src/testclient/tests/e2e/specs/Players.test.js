6f495c79f81340571cb8066c82f446154af71e3a
jon-paul flood <jp.flood@binarydreams.biz>
2020-11-25 18:16:37 +0000

    added visibility wait for table row to have consistent test success.

--- src/testclient/tests/e2e/specs/Players.test.js
+++ src/testclient/tests/e2e/specs/Players.test.js
@@ -8,6 +8,9 @@ describe('Players.vue', () => {
 
   it('Displays list of players', () => {
     cy.visit('/players');
+    // wait for 2nd row to appear first before evaluating
+    cy.get('#players-table > :nth-child(2) > :nth-child(1)', { timeout: 10000 }).should('be.visible');
+    // now evaluate the rows count
     cy.get('#players-table').find('tr').should((elements) => {
       expect(elements).to.have.length(4);
     });
